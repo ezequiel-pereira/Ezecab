85854256a2ab3949834f36c14c85936370e4c00e
Ezequiel Pereira <ezequielpereira92@gmail.com>
2023-09-30 23:14:04 -0300

    activities to login and register, button listeners and toolbar

---
+++ app/src/main/java/com/ezedev/app/LoginRegisterActivity.java
@@ -0,0 +1,22 @@
+package com.ezedev.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+
+public class LoginRegisterActivity extends AppCompatActivity {
+
+    Toolbar mToolbar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_register);
+        mToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        //getSupportActionBar().setTitle("Seleccionar una opción");
+        //No title
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+}
--- app/src/main/java/com/ezedev/app/MainActivity.java
+++ app/src/main/java/com/ezedev/app/MainActivity.java
@@ -2,13 +2,41 @@ package com.ezedev.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button mButtonIAmDriver;
+    Button getmButtonIAmPassenger;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mButtonIAmDriver = findViewById(R.id.btnIAmDriver);
+        getmButtonIAmPassenger = findViewById(R.id.btnIAmPassenger);
+
+        mButtonIAmDriver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToLoginregister();
+            }
+        });
+
+        getmButtonIAmPassenger.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToLoginregister();
+            }
+        });
+    }
+
+    private void goToLoginregister() {
+        Intent intent = new Intent(MainActivity.this, LoginRegisterActivity.class);
+        startActivity(intent);
     }
 }
