1122d3b7bedca542fbbcbc5ab8b1f795c5db5eb6
Ezequiel Pereira <ezequielpereira92@gmail.com>
2023-10-22 21:18:59 -0300

    add logged driver and passenger activities

---
+++ ezecab/src/main/java/com/ezedev/ezecab/activities/driver/DriverMapActivity.java
@@ -0,0 +1,16 @@
+package com.ezedev.ezecab.activities.driver;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ezedev.ezecab.R;
+
+public class DriverMapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver_map);
+    }
+}
---
+++ ezecab/src/main/java/com/ezedev/ezecab/activities/passenger/PassengerMapActivity.java
@@ -0,0 +1,16 @@
+package com.ezedev.ezecab.activities.passenger;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ezedev.ezecab.R;
+
+public class PassengerMapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_passenger_map);
+    }
+}
