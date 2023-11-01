1a9d2ffbc8031d398459cc4e3e03f5f6e509df53
Ezequiel Pereira <ezequielpereira92@gmail.com>
2023-10-31 22:35:57 -0300

    add map to driver and passenger activities

--- ezecab/src/main/java/com/ezedev/ezecab/activities/driver/DriverMapActivity.java
+++ ezecab/src/main/java/com/ezedev/ezecab/activities/driver/DriverMapActivity.java
@@ -1,29 +1,41 @@
 package com.ezedev.ezecab.activities.driver;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
 import com.ezedev.ezecab.R;
 import com.ezedev.ezecab.activities.MainActivity;
 import com.ezedev.ezecab.activities.passenger.PassengerMapActivity;
+import com.ezedev.ezecab.includes.MyToolbar;
 import com.ezedev.ezecab.providers.AuthProvider;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 
-public class DriverMapActivity extends AppCompatActivity {
-    Button mButtonLogout;
+public class DriverMapActivity extends AppCompatActivity implements OnMapReadyCallback {
+    //Button mButtonLogout;
     AuthProvider mAuthProvider;
+
+    private GoogleMap mMap;
+    private SupportMapFragment mMapFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driver_map);
 
-        mButtonLogout = findViewById(R.id.btnLogout);
+        MyToolbar.show(this, "Conductor", false);
+
+        //mButtonLogout = findViewById(R.id.btnLogout);
         mAuthProvider = new AuthProvider();
-        mButtonLogout.setOnClickListener(new View.OnClickListener() {
+        /*mButtonLogout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 mAuthProvider.logout();
@@ -31,6 +43,35 @@ public class DriverMapActivity extends AppCompatActivity {
                 startActivity(intent);
                 finish();
             }
-        });
+        });*/
+        mMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.driver_map);
+        mMapFragment.getMapAsync(this);
+    }
+
+    void logout() {
+        mAuthProvider.logout();
+        Intent intent = new Intent(DriverMapActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.driver_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.action_logout) {
+            logout();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
     }
 }
--- ezecab/src/main/java/com/ezedev/ezecab/activities/passenger/PassengerMapActivity.java
+++ ezecab/src/main/java/com/ezedev/ezecab/activities/passenger/PassengerMapActivity.java
@@ -1,5 +1,6 @@
 package com.ezedev.ezecab.activities.passenger;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -10,19 +11,23 @@ import android.widget.Button;
 import com.ezedev.ezecab.R;
 import com.ezedev.ezecab.activities.MainActivity;
 import com.ezedev.ezecab.providers.AuthProvider;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 
-public class PassengerMapActivity extends AppCompatActivity {
+public class PassengerMapActivity extends AppCompatActivity implements OnMapReadyCallback {
     Button mButtonLogout;
     AuthProvider mAuthProvider;
-
+    private GoogleMap mMap;
+    private SupportMapFragment mMapFragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_passenger_map);
 
-        mButtonLogout = findViewById(R.id.btnLogout);
+        //mButtonLogout = findViewById(R.id.btnLogout);
         mAuthProvider = new AuthProvider();
-        mButtonLogout.setOnClickListener(new View.OnClickListener() {
+        /*mButtonLogout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 mAuthProvider.logout();
@@ -30,6 +35,14 @@ public class PassengerMapActivity extends AppCompatActivity {
                 startActivity(intent);
                 finish();
             }
-        });
+        });*/
+        mMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.driver_map);
+        mMapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
     }
 }
