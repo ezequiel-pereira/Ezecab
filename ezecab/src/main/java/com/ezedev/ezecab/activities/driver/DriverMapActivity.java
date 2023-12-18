4c7131b7e51b39e11a309715e8893bfd5030a7f8
Ezequiel Pereira <ezequielpereira92@gmail.com>
2023-12-17 22:33:13 -0300

    get driver real time location

--- ezecab/src/main/java/com/ezedev/ezecab/activities/driver/DriverMapActivity.java
+++ ezecab/src/main/java/com/ezedev/ezecab/activities/driver/DriverMapActivity.java
@@ -1,21 +1,27 @@
 package com.ezedev.ezecab.activities.driver;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Looper;
+import android.provider.Settings;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.ezedev.ezecab.R;
 import com.ezedev.ezecab.activities.MainActivity;
@@ -30,8 +36,11 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class DriverMapActivity extends AppCompatActivity implements OnMapReadyCallback {
     //Button mButtonLogout;
@@ -41,12 +50,28 @@ public class DriverMapActivity extends AppCompatActivity implements OnMapReadyCa
     private LocationRequest mlocationRequest;
     private FusedLocationProviderClient mFusedLocation;
     private final static int LOCATION_REQUEST_CODE = 1;
+    private final static int SETTINGS_REQUEST_CODE = 2;
+    private Marker mMarker;
+
+    private Button mBtnConnect;
+    private Boolean mConnected;
 
     LocationCallback mLocationCallback = new LocationCallback() {
         @Override
         public void onLocationResult(@NonNull LocationResult locationResult) {
-            for (Location location: locationResult.getLocations()) {
+            for (Location location : locationResult.getLocations()) {
                 if (getApplicationContext() != null) {
+
+                    if (mMarker != null) {
+                        mMarker.remove();
+                    }
+
+                    mMarker = mMap.addMarker(new MarkerOptions()
+                            .position(new LatLng(location.getLatitude(), location.getLongitude()))
+                            .title("Vos")
+                            .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_uber_car))
+                    );
+
                     mMap.moveCamera(CameraUpdateFactory.newCameraPosition(
                             new CameraPosition.Builder()
                                     .target(new LatLng(location.getLatitude(), location.getLongitude()))
@@ -80,12 +105,33 @@ public class DriverMapActivity extends AppCompatActivity implements OnMapReadyCa
 
         mMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.driver_map);
         mMapFragment.getMapAsync(this);
+
+        mBtnConnect = findViewById(R.id.btnConnect);
+        mBtnConnect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mConnected) {
+                    disconnect();
+                } else {
+                    startLocation();
+                }
+            }
+        });
+    }
+
+    private void disconnect() {
+        mBtnConnect.setText("Conectar");
+        mConnected = false;
+        if (mFusedLocation != null){
+            mFusedLocation.removeLocationUpdates(mLocationCallback);
+        }
     }
 
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
         mMap = googleMap;
         mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        //mMap.setMyLocationEnabled(true); //Muestra el punto de ubicación exacta
 
         mlocationRequest = new com.google.android.gms.location.LocationRequest();
         mlocationRequest.setInterval(1000);
@@ -95,6 +141,7 @@ public class DriverMapActivity extends AppCompatActivity implements OnMapReadyCa
 
         startLocation();
     }
+
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
@@ -119,26 +166,64 @@ public class DriverMapActivity extends AppCompatActivity implements OnMapReadyCa
                                 ActivityCompat.requestPermissions(DriverMapActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_REQUEST_CODE);
                             }
                         });
-            }
-            else {
+            } else {
                 ActivityCompat.requestPermissions(DriverMapActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_REQUEST_CODE);
             }
         }
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == SETTINGS_REQUEST_CODE && checkGpsActive()) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            mFusedLocation.requestLocationUpdates(mlocationRequest, mLocationCallback, Looper.myLooper());
+        } else {
+            showLocationSettingsAlertDialog();
+        }
+    }
+
+    private void showLocationSettingsAlertDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Activar ubicación")
+                .setPositiveButton("Configuración", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        startActivityForResult(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS), SETTINGS_REQUEST_CODE);
+                    }
+                }).create().show();
+    }
+
+    private boolean checkGpsActive() {
+        boolean isActive = false;
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            isActive = true;
+        }
+        return isActive;
+    }
+
     private void startLocation() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-                mFusedLocation.requestLocationUpdates(mlocationRequest, mLocationCallback, Looper.myLooper());
-            }
-            else {
+                if (checkGpsActive()) {
+                    mBtnConnect.setText("Desconectar");
+                    mConnected = true;
+                    mFusedLocation.requestLocationUpdates(mlocationRequest, mLocationCallback, Looper.myLooper());
+                } else {
+                    showLocationSettingsAlertDialog();
+                }
+            } else {
                 checkLocationPermission();
             }
-        }
-        else {
+        } else {
             mFusedLocation.requestLocationUpdates(mlocationRequest, mLocationCallback, Looper.myLooper());
         }
     }
+
     void logout() {
         mAuthProvider.logout();
         Intent intent = new Intent(DriverMapActivity.this, MainActivity.class);
